a07ef5b9eb104b89ce1bbb50e284d9beef8b9425
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2021-02-03 18:04:10 -0500

    add question21 to python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -1,3 +1,5 @@
+import math
+
 def question1():
     """
     Auteur : Charlotte Foucard
@@ -12,6 +14,41 @@ def question1():
 
     print(','.join(l))
 
+def question21():
+    """
+    Auteur: Marc-Antoine Ricard
+    Description: program that can get command like UP 5, DOWN 3, LEFT 3, RIGHT 2 and calculate optimale distance between the starting point and the ending point
+    Test:
+    UP 5
+    DOWN 3
+    LEFT 3
+    RIGHT 2
+
+    should give: 
+    2
+    """
+    pos = {'x':0, 'y':0}
+    while True:
+        dir = input()
+        try:
+            dir = dir.split()
+            commande = dir[0]
+            movement = int(dir[1])
+
+            if commande == "UP":
+                pos['y'] += movement
+            if commande == "DOWN":
+                pos['y'] -= movement
+            if commande == "LEFT":
+                pos['x'] += movement
+            if commande == "RIGHT":
+                pos['x'] -= movement
+        except:
+            #its not a real command, finish the algorythm
+            break
+
+    #distance btw 2 points: √((x2−x1)^2 + (y2−y1)^2)
+    print( round(math.sqrt( (pos['x']**2) + (pos['y']**2))))
 
 # Test question 1 
 # ce test est a titre d'exemple
