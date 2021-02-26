335761671e346672fe8df859129a5f803d6aabcf
Marc-Antoine Ricard <marcantoinericard@gmail.com>
2021-02-26 14:50:29 -0500

    add question 22

--- python_challenge.py
+++ python_challenge.py
@@ -51,7 +51,37 @@ def question21():
     #distance btw 2 points: √((x2−x1)^2 + (y2−y1)^2)
     print( round(math.sqrt( (pos['x']**2) + (pos['y']**2))))
 
+def question22():
+    """
+    Auteur: Marc-Antoine Ricard
+    Description:
+    Write a program to compute the frequency of the words from the input. The output should output after sorting the key alphanumerically.
+    Suppose the following input is supplied to the program:
+    New to Python or choosing between Python 2 and Python 3? Read Python 2 or Python 3.
+    Then, the output should be:
+    2:2
+    3.:1
+    3?:1
+    New:1
+    Python:5
+    Read:1
+    and:1
+    between:1
+    choosing:1
+    or:2
+    to:1
+    """
+    inp = sorted(str(input()).split())
+    answer = dict()
+    for word in inp:
+        if word in answer:
+            answer[word] += 1
+        else:
+            answer[word] = 1
+    for key, value in answer.items():
+        print(f'{key}:{value}')
+
 # Test question 1 
 # ce test est a titre d'exemple
 # On ne devrais pas garder ca, car ca va faire beaucoup de print si tout le monde en fait.
-question1()
+question22()
